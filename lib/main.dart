14f6534f397f0bfba5c4b8d7c2dd79e8b3101e34
Nimantha Karunanayake <nimanthakmp@gmail.com>
2023-05-16 23:58:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,80 +11,61 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        //appBar: AppBar(),
         backgroundColor: Colors.purple,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage("images/nimantha.jpg"),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                "Nimantha",
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    //fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-              Text(
-                "FLUTTER DEVELOPER",
-                style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    letterSpacing: 2.5),
-              ),
-              SizedBox(
-                height: 20,
-                width: 150,
-                child: Divider(
-                  color: Colors.white54,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/nimantha.jpg'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Nimantha',
+              style: TextStyle(
+
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'Pacifico'),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 20, width: 150,
+            child: Divider(color: Colors.white54,),),
+
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30,),
+              child: ListTile(
+                leading: Icon(Icons.phone),
+                title: Text(
+                  '+94 710 744 814',
+                  style: TextStyle(fontSize: 15),
                 ),
               ),
-              Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  //padding: const EdgeInsets.all(10) ,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.purple,
-                    ),
-                    title: Text(
-                      "+94 710 744 814",
-                      style: TextStyle(
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  )),
-              Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  //padding: const EdgeInsets.all(10) ,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.purple,
-                    ),
-                    title: Text(
-                      "nimanthakmp@gmail.com",
-                      style: TextStyle(
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  )),
-            ],
-          ),
-        ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30,),
+              child: ListTile(
+                leading: Icon(Icons.mail),
+                title: Text(
+                  'nimanthakmp@gmail.com',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
